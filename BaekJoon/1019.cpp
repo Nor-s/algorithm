15288b64e8fb829455232468a11e479e5441cf95
Nor-s <s28118149@gmail.com>
2021-05-12 21:02:26 +0900

    .

--- BaekJoon/1019.cpp
+++ BaekJoon/1019.cpp
@@ -3,50 +3,70 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <cstring>
 using namespace std;
 
-int number[10] = {0};
+long long number[10] = {0};
 
-void addToNumber(int n, int x) {
+void printNumber() {
+    for(int i = 0; i < 10; i++) {
+        cout<<number[i]<<" ";
+    }
+    cout<<"\n";
+}
+void addToNumber(int n, long long x) {
     number[n] += x;
 }
 
-void addCount(int n, int digit) {
+void addCount(int n, long long digit, int idx) {
     if(n == 0) return;
     for(int i = 0; i < n; i++) {
-        addToNumber(i, pow(10, digit));
-        for(int i = 0; i < 10; i++) {
-            addToNumber(i, (digit)*pow(10,digit-1));
+        addToNumber(i,digit);
+        for(int j = 0; j < 10; j++) {
+            addToNumber(j, (idx)*digit/10);
         }
     }
-    for(int i = 0; i <= digit; i++) {
-        addToNumber(0, -1*pow(10, i));
-    }
+    
     addToNumber(n, 1);
-    addToNumber(0, digit);
-
+    addToNumber(0, idx);
 }
 
 void counting(string& n) {
-    int digit = 1;
+    long long digit = 1;
     for(int i = 0; i < n.size(); i++) {
         int num = (int)n[i] - '0';
-        addCount(num, i);
+        addCount(num, digit ,i);
+        if(i != n.size() -1 && num != 0) {
+            addToNumber(0, -i-1);
+        }
 
         for(int j = i+1; j < n.size(); j++) {
             addToNumber((int)n[j]- '0', num*digit);
         }
+        addToNumber(0, -1*digit);
         digit*= 10;
+    }    
+}
+void converting(int n) {
+    vector<int> countNum(10, 0);
+    for(int i = 1; i <= n; i++) {
+        int tmp = i;
+        while(tmp != 0) {
+            countNum[tmp%10]++;
+            tmp/=10;
+        }
     }
-
+    for(int i = 0; i < 10; i++) {
+        cout<<countNum[i]<<" ";
+    }
+    cout<<"\n";
 }
 
 int main() {
     string n;
     cin>>n;
+    converting(stoi(n));
     reverse(n.begin(), n.end());
     counting(n);
-    for(int i = 0; i < 10; i++) {
-        cout<<number[i]<<" ";
-    }
+    printNumber();
 }
