c7d5b7318b26d44d18e3a6b58ac190444f3d63a6
Nor-s <s28118149@gmail.com>
2021-07-01 21:29:23 +0900

    bsearch, two pointer

--- BaekJoon/2473.cpp
+++ BaekJoon/2473.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-
+ 
 using namespace std;
-
+ 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
@@ -13,29 +13,27 @@ int main() {
     cin>>N;
     vector<long long> s(N);
     long long answer = 3000000001;
-
+ 
     for(int i = 0; i < N; i++) {
         cin>>s[i];
     }
     sort(s.begin(), s.end());
-
+ 
     long long abc[3] = {s[0], s[1], s[2]};
-
+ 
     for(int i = 0; i < N; i++) {
        for(int j = i + 1; j < N; j++) {
            long long sum = s[i] + s[j];
            auto lo = lower_bound(s.begin() + j + 1, s.end(), -sum);
-           auto up = upper_bound(s.begin() + j + 1, s.end(), -sum);
-           int test[3] = {(int) (lo - s.begin()),
-                          (int) (up - s.begin()),
+           int test[2] = {(int) (lo - s.begin()),
                           (int) (lo - 1 - s.begin())};
           
-           for(int k = 0; k < 3; k++) {
+           for(int k = 0; k < 2; k++) {
                int idx = test[k];
                if(idx < 0 || idx >= N) {
                    continue;
                }
-               if(answer > abs(sum + s[idx]) && idx != i && idx != j) {
+               if(answer > abs(sum + s[idx]) && idx != j) {
                    answer = abs(sum + s[idx]);
                    abc[0] = s[i];
                    abc[1] = s[j];
