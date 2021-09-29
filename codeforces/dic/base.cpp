7a2d2397b0f7e6ac4f703605c7fa63abb0552877
Nor-s <s28118149@gmail.com>
2021-09-29 12:10:42 +0900

    Add folder

---
+++ codeforces/dic/base.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int T;
+    cin >> T;
+    while (T)
+    {
+        }
+}
